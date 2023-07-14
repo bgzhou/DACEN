6a9188a75a8a51e4d823915393c42debe18d9e6c
Binggui <binggui.zhou@connect.um.edu.mo>
2023-07-14 22:36:55 +0800

    improve efficiency of evaluating cosine similarity

--- trainer_TL_target.py
+++ trainer_TL_target.py
@@ -53,69 +53,55 @@ ht = np.load("PATH_TO_THE_CHANNEL_LABEL")
 split_valid_idx, split_test_idx = int(0.7 * pilot.shape[0]), int(0.8 * pilot.shape[0])
 pilot_train, pilot_valid, pilot_test = pilot[:split_valid_idx,...], pilot[split_valid_idx:split_test_idx,...],  pilot[split_test_idx:,...]
 ht_train, ht_valid, ht_test = ht[:split_valid_idx,...], ht[split_valid_idx:split_test_idx,...],  ht[split_test_idx:,...]
-
+##############################################################################################
 # process training data (pilot, channel, and cos similarity)
-index_list = [[0,4],[2,6]]
-train_pilots = []
-train_labels = []
-instance_weights = []
+ref_indices = [9,17]
+index_list = [[8,16],[10,18]]
 
-num_total_samples = 0
+threshold = 0.9
 _total_samples = []
 _total_sample_weights = []
 _total_sample_labels = []
 for indices in index_list:
     _pilot_train = pilot_train[..., indices]
-    _pilot_train = rearrange(_pilot_train, 'b (c rx) (subc sym) rb -> b (subc sym) (c rx rb)', c=2, subc=8)
-    ref_pilot_train = pilot_train[..., [1,5]]
-    ref_pilot_train = rearrange(ref_pilot_train, 'b (c rx) (subc sym) rb -> b (subc sym) (c rx rb)', c=2, subc=8)
-    img_total = 2 * 4 * len(index_list[0])
-    num_sample_subband = 8 * 4
+    _pilot_train = rearrange(_pilot_train, 'b (c rx) (subc sym) rb -> b c rb (rx subc sym)', c=2, subc=8)
+    _pilot_train = _pilot_train[:,0,...] + 1j*_pilot_train[:,1,...]
+    ref_pilot_train = pilot_train[..., ref_indices]
+    ref_pilot_train = rearrange(ref_pilot_train, 'b (c rx) (subc sym) rb -> b c rb (rx subc sym)', c=2, subc=8)
+    ref_pilot_train = ref_pilot_train[:,0,...] + 1j*ref_pilot_train[:,1,...]
+    Nv = len(ref_indices)
     for j, (_pilot, _ref_pilot) in enumerate(zip(_pilot_train, ref_pilot_train)):
-        _ref_pilot2 = _ref_pilot[0:num_sample_subband, 0:int(img_total):2] + 1j*_ref_pilot[0:num_sample_subband, 1:int(img_total):2]
-        _pilot2 = _pilot[0:num_sample_subband, 0:int(img_total):2] + 1j*_pilot[0:num_sample_subband, 1:int(img_total):2]
         score_cos = 0
-        for i in range(num_sample_subband):
-            _ref_pilot2_sample = _ref_pilot2[i:i+1,]
-            _pilot2_sample = _pilot2[i:i+1,]
-            score_tmp = cos_sim(_ref_pilot2_sample,_pilot2_sample)
-            score_cos = score_cos + abs(score_tmp)*abs(score_tmp)
-        score_cos = score_cos/num_sample_subband
-        if score_cos >= 0.9:
-            num_total_samples += 1
-            _total_samples.append(rearrange(_pilot, '(subc sym) (c rx rb) -> 1 c rx (rb subc) sym', c=2, rx =4, subc=8))
+        for i in range(Nv):
+            _ref_pilot_sample = _ref_pilot[i]
+            _pilot_sample = _pilot[i]
+            score_tmp = cos_sim(_ref_pilot_sample,_pilot_sample)
+            # print(abs(score_tmp))
+            score_cos = score_cos + abs(score_tmp)
+        score_cos = score_cos/Nv
+        if score_cos >= threshold:
+            _total_samples.append(rearrange(_pilot, 'rb (rx subc sym) -> 1 rb (rx subc sym)', rx=4, subc=8))
             _total_sample_weights.append(score_cos)
             _total_sample_labels.append(ht_train[j:j+1,...])
-    train_pilots.append(np.concatenate(_total_samples))
-    train_labels.append(np.concatenate(_total_sample_labels))
-    instance_weights.append(np.array(_total_sample_weights))
 
-index_list = [[1,5]]
-for indices in index_list:
-    _pilot_train = pilot_train[..., indices]
-    _pilot_train = rearrange(_pilot_train, 'b (c rx) (subc sym) rb -> b c rx (rb subc) sym', c=2, subc=8)
-    train_pilots.append(_pilot_train)
-    train_labels.append(ht_train)
-    instance_weights.append(np.ones(ht_train.shape[0]))
+ht_train = np.concatenate([ht_train, np.concatenate(_total_sample_labels)])
+instance_weights = np.concatenate([np.ones(ht_train.shape[0]), np.array(_total_sample_weights)])
 
-pilot_train = np.concatenate(train_pilots)
-ht_train = np.concatenate(train_labels)
-instance_weights = np.concatenate(instance_weights)
+_pilot_sim = np.expand_dims(np.concatenate(_total_samples), axis=1)
+_pilot_sim = np.concatenate([np.real(_pilot_sim), np.imag(_pilot_sim)], 1)
+_pilot_sim = rearrange(_pilot_sim, 'b c rb (rx subc sym) -> b c rx (rb subc) sym', rx=4, subc=8)
+
+_pilot_train = pilot_train[..., ref_indices]
+_pilot_train = rearrange(_pilot_train, 'b (c rx) (subc sym) rb -> b c rx (rb subc) sym', rx=4, subc=8)
+pilot_train = np.concatenate([_pilot_train, _pilot_sim])
+
 train_dataset = DatasetFolder_weights(pilot_train, ht_train, instance_weights)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=False)
 
 # process validation data (pilot, channel)
-index_list = [[1,5]]
-valid_pilots = []
-valid_labels = []
-for indices in index_list:
-    _pilot_valid = pilot_valid[..., indices]
-    _pilot_valid = rearrange(_pilot_valid, 'b (c rx) (subc sym) rb -> b c rx (rb subc) sym', c=2, subc=8)
-    valid_pilots.append(_pilot_valid)
-    valid_labels.append(ht_valid)
+_pilot_valid = pilot_valid[..., ref_indices]
+pilot_valid = rearrange(_pilot_valid, 'b (c rx) (subc sym) rb -> b c rx (rb subc) sym', rx=4, subc=8)
 
-pilot_valid = np.concatenate(valid_pilots)
-ht_valid = np.concatenate(valid_labels)
 valid_dataset = DatasetFolder(pilot_valid, ht_valid)
 valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=False)
 
